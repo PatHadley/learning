ad4c417efff6237cdba6ad2f1e0a8790ba4a2353
Pat Hadley <pat@pathadley.net>
2016-09-23 12:19:31 +0100

    baby steps

---
+++ PHP/chrome-ext/art-tab/rijks-pics.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Results?</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<p>
+			Your search was: <?php echo $_POST["search"]; ?>
+		</p>
+	</body>
+</html>
+
