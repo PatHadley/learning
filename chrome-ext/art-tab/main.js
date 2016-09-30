6f8985eebe4f92254fa046a1e1e8ba5ad27e181e
Pat Hadley <pat@pathadley.net>
2016-09-30 16:21:58 +0100

    progress in back and front-end

---
+++ chrome-ext/art-tab/main.js
@@ -0,0 +1,6 @@
+$(function(){
+
+  $('img').attr("src", "http://lh4.ggpht.com/zdulZIXgJzad3nA2llXCaKNjFtVsmplAA20IM_SnC2MjNvwxA841YvmrYTylvLP_AAmhO5uLpnIsgoRmzUn9BBHZ2-M9=s0");
+
+
+});
--- chrome-ext/art-tab/scrapers/rijks1.js
+++ chrome-ext/art-tab/scrapers/rijks1.js
@@ -4,22 +4,78 @@ const unirest = require('unirest');
 
 console.log ("I'm working!");
 
-unirest.get("https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=w5b8sjYU&format=json")
-.type('json')
-.end(function(response){
-  // console.log(response.body.artObject.title);
-
-});
-
-unirest.get("https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=w5b8sjYU&format=json")
-.type('json')
-.end(function(response){
-  // console.log(response.body.artObject.title);
-  var output = response.body.artObject.title;
-  return output;
-});
+var objectNums = [];
 
 
-// var output = unirest.get("https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=w5b8sjYU&format=json").type('json').end(function(response){response.body});
+var writeArray = function(objectNums){
+  fs.writeFile('OilPaintingObjectNums.json', "["+objectNums+"]", function(err){
+    if (err) return console.log(err);
+  });
+};
 
-console.log(rijks());
+
+
+var processRijks = function(output){
+  if (output){
+    // console.log(output.title);
+    // console.log(output.objectNumber);
+    objectNums.push('\n"'+output.objectNumber+'"');
+    writeArray(objectNums);
+  };
+};
+
+
+var getRijks = function (){
+
+  for (var i = 1; i <= 1000; i++) {
+    unirest.get("https://www.rijksmuseum.nl/api/en/collection/sk-c-"+i+"?key=w5b8sjYU&format=json&imgonly=true")
+    .type('json')
+    .end(function(response){
+      var output = response.body.artObject;
+      processRijks(output);
+    });
+  }
+
+  
+};
+
+
+getRijks();
+
+
+
+
+
+
+var collPrefixes = [
+  "AK-MAK",
+  "AK-NM",
+  "AK-RAK",
+  "AK-RBK",
+  "BK",
+  "BK-AM",
+  "BK-KOG",
+  "BK-NM",
+  "NG",
+  "NG-NM",
+  "NG-VG",
+  "SK-A",
+  "SK-B",
+  "SK-C",
+  "RP-F",
+  "RP-P",
+  "RP-P-OB",
+  "RP-T"
+];
+
+// var years = [];
+
+// for (var i = 0; i <= 115; i++) {
+//   years.push(i+1900);
+// };
+
+// var hundreds = [];
+
+// for (var i = 1; i <= 100; i++){
+//   hundreds.push(i);
+// };
