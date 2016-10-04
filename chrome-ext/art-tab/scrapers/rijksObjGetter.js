64ef5fb224de49c74d3b1adff452b0b0f238c757
Pat Hadley <pat@pathadley.net>
2016-10-04 10:18:49 +0100

    it works with 50k objects

--- chrome-ext/art-tab/scrapers/rijksObjGetter.js
+++ chrome-ext/art-tab/scrapers/rijksObjGetter.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const unirest = require('unirest');
+// const https = require('https');
+// const http = require('http');
 
 
 console.log ("I'm working!");
@@ -8,7 +10,7 @@ var objectDetails = [];
 
 
 var writeArray = function(objectDetails){
-  fs.writeFile('scrapers/rijksObjects.json', "["+objectDetails+"]", function(err){
+  fs.appendFile('scrapers/rijksObjects.json', "["+objectDetails+"]", function(err){
     if (err) return console.log(err);
   });
 };
@@ -22,6 +24,7 @@ var processRijks = function(output){
       creator: output.principalMakers[0].name,
       imgUrl: output.webImage.url
     };
+    console.log("Worked with: "+currentObject);
     currentObjString = JSON.stringify(currentObject);
     objectDetails.push('\n'+currentObjString);
     writeArray(objectDetails);
@@ -33,7 +36,7 @@ var processRijks = function(output){
       creator: "anonymous",
       imgUrl: output.webImage.url
     };
-    // console.log(currentObject);
+    console.log("Worked with: "+currentObject);
     currentObjString = JSON.stringify(currentObject);
     objectDetails.push('\n'+currentObjString);
     writeArray(objectDetails);
@@ -45,25 +48,61 @@ var getRijks = function (){
   var objectsString = fs.readFileSync('scrapers/rijksNumbers.json', 'utf8');
   objectsArray = JSON.parse(objectsString);
 
-  for (var i = (objectsArray.length /10)- 1; i >= 0; i--) {
-    // console.log("https://www.rijksmuseum.nl/api/en/collection/"+objectsArray[i]+"?key=w5b8sjYU&format=json&imgonly=true");
+  for (var i = 0; i < (objectsArray.length/10); i++) {
+
+     // console.log("https://www.rijksmuseum.nl/api/en/collection/"+objectsArray[i]+"?key=w5b8sjYU&format=json&imgonly=true");
+
+  //   var options = {
+  //     hostname: "www.rijksmuseum.nl",
+  //     path: "/api/en/collection/"+objectsArray[i]+"?key=w5b8sjYU&format=json&imgonly=true",
+  //     method: 'GET'
+  //   };
+
+  //   var req = https.request(options, (res) => {
+  //     console.log('statusCode:', res.statusCode);
+  //     res.setEncoding('utf8');
+
+  //     res.on('data', (d) => {
+  //       // process.stdout.write(d);
+  //       response = (`BODY: ${d}`);
+  //       console.log(response);
+  //       // if ((response.body === null) || (response.body === undefined)){
+  //       //   console.log("empty response");
+  //       // } else if ((response.body.artObject === null) || (response.body.artObject === undefined)){
+  //       //   console.log("no artObject");
+  //       // } else if (response.body.artObject.webImage){
+  //       //   var output = response.body.artObject;
+  //       //   processRijks(output);
+  //       // };
+  //     });
+  //   });
+  //   req.end();
+
+  //   req.on('error', (e) => {
+  //     console.log(`problem with request: ${e.message}`);
+  //   });
+
+
+
     unirest.get("https://www.rijksmuseum.nl/api/en/collection/"+objectsArray[i]+"?key=w5b8sjYU&format=json&imgonly=true")
     .type('json')
     .end(function(response){
       if (!response.ok){
-        console.log("Weird status: "+response);
+
+        var status = JSON.stringify(response);
+        console.log("Weird status: "+status);
         console.log("Status: "+response.statusType);
       } else if ((response.body === null) || (response.body === undefined)){
-        // console.log("empty response");
+        console.log("empty response");
       } else if ((response.body.artObject === null) || (response.body.artObject === undefined)){
-        // console.log("no artObject");
+        console.log("no artObject");
       } else if (response.body.artObject.webImage){
         var output = response.body.artObject;
         processRijks(output);
       };
     });
   };
-}; 
+};
 
 
 
