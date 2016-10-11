6865125bbf21712bce65a566838c605d2faeb4b7
Pat Hadley <pat@pathadley.net>
2016-10-11 12:49:46 +0100

    tate test works

--- chrome-ext/art-tab/js/main.js
+++ chrome-ext/art-tab/js/main.js
@@ -8,7 +8,8 @@ $(function(){
 
   var randomNum = getRandomInt(0, artefacts.length);
 
-  var randomArtefact = $(artefacts[randomNum]);
+  // var randomArtefact = $(artefacts[randomNum]);
+  var randomArtefact = $(tatefacts[randomNum]);
 
   artefactToLoad = '<div id="labels"><h1><a href="https://www.rijksmuseum.nl/en/collection/'+randomArtefact[0].id+'">'+randomArtefact[0].title+'</a></h1><p><a href="https://www.rijksmuseum.nl/en/search?p=1&ps=12&involvedMaker='+randomArtefact[0].creator+'&st=OBJECTS">'+randomArtefact[0].creator+'</a></p></div>'
 
